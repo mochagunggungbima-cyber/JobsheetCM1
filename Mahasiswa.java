9250482f120c4def26a1352c5dfed6bd3acbb984
Moch.Agung Bima Ardian <bimaagungbimzz@gmail.com>
2026-04-08 11:12:47 +0700

    1

---
+++ Mahasiswa.java
@@ -0,0 +1,19 @@
+public class Mahasiswa {
+    String nim;
+    String nama;
+    String prodi;
+
+    public Mahasiswa() {}
+
+    public Mahasiswa(String nim, String nama, String prodi) {
+        this.nim   = nim;
+        this.nama  = nama;
+        this.prodi = prodi;
+    }
+
+    public void tampilInformasi() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Prodi : " + prodi);
+    }
+}
